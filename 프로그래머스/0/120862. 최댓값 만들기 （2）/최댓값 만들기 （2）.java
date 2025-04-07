ca0adf52080b26c506d890fa4c75dc50d64fde4d
김민호 <150920427+minho-git@users.noreply.github.com>
2025-04-07 13:42:06 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.53 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,13 +1,11 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] numbers) {
-        int max = numbers[0] * numbers[1];
-        for(int i = 0; i < numbers.length; i++){
-            for(int j = i+1; j < numbers.length; j++){
-                if(max < numbers[i] * numbers[j]){
-                    max = numbers[i] * numbers[j];
-                }
-            }
-        }
+        
+        Arrays.sort(numbers);
+        int len = numbers.length;
+        int max = Math.max(numbers[0] * numbers[1], numbers[len - 1] * numbers[len - 2]);
         
         return max;
     }
