602efb224e60254dd99b6eb79acd807bd8f3e665
김민호 <150920427+minho-git@users.noreply.github.com>
2025-04-08 13:45:30 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.07 ms, Memory: 87.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> result = new ArrayList();
+        for(int i = 1; i < n + 1; i++) {
+            if(i % 2 != 0) {
+                result.add(i);
+            }
+        }
+        
+        int[] answer = new int[result.size()];
+        for(int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+
+        return answer;
+    }
+}
