fcbe4b16a21c3335710956c1b02ba2a9ba40791e
김민호 <150920427+minho-git@users.noreply.github.com>
2025-04-08 14:34:20 +0900

    [level 0] Title: 숫자 찾기, Time: 9.51 ms, Memory: 82.1 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,13 +1,6 @@
 class Solution {
     public int solution(int num, int k) {
-        String num1 = num + "";
-        String k1 = k + "";
         
-        if(!num1.contains(k1)) {
-            return -1;
-        }
-        
-        int answer = num1.indexOf(k1) + 1;
-        return answer;
+        return (" " + num).indexOf(k + "");
     }
 }
