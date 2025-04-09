8be97e5592e215bb5ece40942685f88b1b69dac6
김민호 <150920427+minho-git@users.noreply.github.com>
2025-04-09 10:17:37 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(String s) {
+        
+        char[] array = s.toCharArray();
+        if(!(array.length == 4 || array.length == 6)){
+            return false;
+        }
+        for(char a : array) {
+            if(a < 48 || a > 57) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
