6e2ed9bcdcc3b604f7eb15335a86c9fbf1697d3f
김민호 <150920427+minho-git@users.noreply.github.com>
2025-04-09 09:49:46 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.59 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
+++ 프로그래머스/1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.java
@@ -4,18 +4,36 @@ class Solution {
     public String solution(String s) {
         
         char[] c = s.toCharArray();
-        char tmp = ' ';
-        for(int i = 0; i < c.length - 1; i++) {
-            for(int j = i + 1; j < c.length; j++) {
-                if(c[i] < c[j]) {
-                    tmp = c[i];
-                    c[i] = c[j];
-                    c[j] = tmp;
-                }
-            }
-        }
+        Arrays.sort(c);
         
-        String answer = String.valueOf(c);
+        String answer = (new StringBuilder(new String(c)).reverse().toString());
+        
+//         List<Character> list = new ArrayList();
+//         for(int i = 0; i < c.length; i++) {
+//             list.add(c[i]);
+//         }
+//         Collections.sort(list);
+//         Collections.reverse(list);
+
+        
+//         String answer = "";
+//         for(Character cc : list) {
+//             answer += cc;
+//         }
+
+        
+        // char tmp = ' ';
+        // for(int i = 0; i < c.length - 1; i++) {
+        //     for(int j = i + 1; j < c.length; j++) {
+        //         if(c[i] < c[j]) {
+        //             tmp = c[i];
+        //             c[i] = c[j];
+        //             c[j] = tmp;
+        //         }
+        //     }
+        // }
+        
+        // String answer = String.valueOf(c);
         
         return answer;
     }
