0562479273ca4cf35448dc673fe14f8e0f7f04dc
김민호 <150920427+minho-git@users.noreply.github.com>
2025-04-09 11:29:45 +0900

    [level 0] Title: 외계행성의 나이, Time: 7.65 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -2,14 +2,21 @@ class Solution {
     public String solution(int age) {
     
         String str = age + "";
-        char[] chars = str.toCharArray();
-        for(int i = 0; i < chars.length; i++) {
-            
-            chars[i] = (char)(chars[i] + ('a' - '0'));
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < str.length(); i++) {
+            sb.append((char)(str.charAt(i) + ('a' - '0')));
         }
         
+        String answer = sb.toString();
+//         char[] chars = str.toCharArray();
+//         for(int i = 0; i < chars.length; i++) {
+            
+//             chars[i] = (char)(chars[i] + ('a' - '0'));
+//         }
         
-        String answer = new String(chars);
-        return answer;
+        
+//         String answer = new String(chars);
+         return answer;
     }
 }
