9b8aa0ce36c65c2af89cd17a795095ea6aba8010
김민호 <150920427+minho-git@users.noreply.github.com>
2025-04-09 13:47:10 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.15 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -4,16 +4,8 @@ class Solution {
         numbers = numbers.replace("zero", "0").replace("one", "1").replace("two", "2").replace("three", "3")
             .replace("four", "4").replace("five", "5").replace("six", "6").replace("seven", "7")
             .replace("eight", "8").replace("nine", "9");
-    
-        char[] nums = numbers.toCharArray();
-        Long answer = 0L;
-        Long count = 1L;
         
-        for(int i = nums.length-1; i >= 0; i--){
-            answer += (nums[i] - '0') * count;
-            count *= 10;
-            
-        }
+        Long answer = Long.parseLong(numbers);
         
         return answer;  
     }
