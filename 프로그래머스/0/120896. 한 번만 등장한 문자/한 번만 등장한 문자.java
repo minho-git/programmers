731f9a56bbea804f434a7bb4f2146c7761596a81
김민호 <150920427+minho-git@users.noreply.github.com>
2025-04-10 12:22:59 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 2.07 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -1,22 +1,50 @@
 import java.util.*;
 class Solution {
     public String solution(String s) {
+        
+        HashSet<String> set = new HashSet();
         String answer = "";
-        HashMap<Character, Integer> map = new HashMap();
-        for(int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        
+        //set에 담고
+        String[] array = s.split("");
+        for(String ss : array) {
+            set.add(ss);
         }
         
-        for(int i = 0; i < s.length(); i++) {
-            if(map.get(s.charAt(i)) == 1) {
-                answer += s.charAt(i);
+        //비교할 배열을 만들고
+        Object[] array2 = set.toArray();
+        Arrays.sort(array2);
+        // System.out.println(Arrays.toString(array2));
+        
+        //원래 문자열을 리스트로 만들어서
+        
+        List<String> list = Arrays.asList(array);
+        
+        for(Object o : array2) {
+            if(Collections.frequency(list, o) == 1) {
+                answer += o;
             }
         }
         
-        char[] array = answer.toCharArray();
-        Arrays.sort(array);
-        answer = new String(array);
+        
+        
+        
+//         String answer = "";
+//         HashMap<Character, Integer> map = new HashMap();
+//         for(int i = 0; i < s.length(); i++) {
+//             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+//         }
+        
+//         for(int i = 0; i < s.length(); i++) {
+//             if(map.get(s.charAt(i)) == 1) {
+//                 answer += s.charAt(i);
+//             }
+//         }
+        
+//         char[] array = answer.toCharArray();
+//         Arrays.sort(array);
+//         answer = new String(array);
 
-        return answer;
+         return answer;
     }
 }
