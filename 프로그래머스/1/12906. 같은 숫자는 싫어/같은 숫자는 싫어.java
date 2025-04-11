dfa201760bf30ac0cab9f6693302411194a9c35f
김민호 <150920427+minho-git@users.noreply.github.com>
2025-04-11 10:30:17 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 48.60 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,22 +2,39 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        List<Integer> list = new ArrayList();
+//         List<Integer> list = new ArrayList();
         
-       for(int i = 0; i < arr.length; i++) {
-           if(i == 0) {
-               list.add(arr[0]);
-           } else {
-               if(arr[i-1] != arr[i]) {
-                   list.add(arr[i]);
-               }
-           } 
-       }
+//        for(int i = 0; i < arr.length; i++) {
+//            if(i == 0) {
+//                list.add(arr[0]);
+//            } else {
+//                if(arr[i-1] != arr[i]) {
+//                    list.add(arr[i]);
+//                }
+//            } 
+//        }
         
         
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+//         int[] answer = new int[list.size()];
+//         for(int i = 0; i < list.size(); i++) {
+//             answer[i] = list.get(i);
+//         }
+        
+//         return answer;
+        
+        Stack<Integer> stack = new Stack();
+        for(Integer num : arr) {
+            if(stack.empty()) {
+                stack.push(num);
+            } else if(stack.peek() != num){
+                stack.push(num);
+            }
+        }
+        
+        
+        int[] answer = new int[stack.size()];
+        for(int i = 0; i < stack.size(); i++) {
+            answer[i] = stack.get(i);
         }
         
         return answer;
