de7771b82cdf2c414b283cf264137c33e9e3233b
김민호 <150920427+minho-git@users.noreply.github.com>
2025-04-11 13:15:06 +0900

    [level 1] Title: 햄버거 만들기, Time: 91.68 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -8,15 +8,15 @@ class Solution {
         for(int i = 0; i < ingredient.length; i++) {
             stack.push(ingredient[i]);
             last++;
-            if(stack.size() >= 4) {
-                if(stack.get(last-1) == 1 && stack.get(last-2) == 3 &&  stack.get(last-3) == 2 && stack.get(last-4) == 1) {
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                    stack.pop();
-                    last = last - 4;
-                    answer++;
-                }
+            if(stack.size() >= 4 && stack.get(last-1) == 1 && stack.get(last-2) == 3 
+               &&  stack.get(last-3) == 2 && stack.get(last-4) == 1) {
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                stack.pop();
+                last = last - 4;
+                answer++;
+                    
             } 
         }
 
