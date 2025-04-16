665326ac4e6ca4b519d18303a9a22f88c5e9ebf8
김민호 <150920427+minho-git@users.noreply.github.com>
2025-04-16 11:46:05 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 920 ms, Memory: 90196 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M, R;
+    static boolean[] visited;
+    static int[] answer;
+    static ArrayList<Integer>[] graph;
+    static int order = 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N + 1];
+        answer = new int[N + 1];
+        graph = new ArrayList[N+1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph[i]);
+            Collections.reverse(graph[i]);
+        }
+
+        dfs(R);
+
+        for (int i = 1; i <= N; i++) {
+            bw.write(String.valueOf(answer[i]));
+            bw.newLine();
+        }
+
+        br.close();
+        bw.close();
+
+    }
+
+    private static void dfs(int idx) {
+        visited[idx] = true;
+        answer[idx] = order;
+        order++;
+
+        for (int i = 0; i < graph[idx].size(); i++) {
+            if (visited[graph[idx].get(i)] == false) {
+                dfs(graph[idx].get(i));
+            }
+
+        }
+
+    }
+
+
+}
