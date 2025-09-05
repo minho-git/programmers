f3c5f1199da34a55c23a35c249e8e941818a238f
김민호 <150920427+minho-git@users.noreply.github.com>
2025-09-05 15:23:57 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -5,15 +5,11 @@ for i in range(C):
     array = list(map(int, input().split()))
     people = array[0]
 
-    sum = 0
-    for j in range(1, people + 1):
-        sum += array[j]
-
-    avg = sum / people
+    avg = sum(array[1:])/array[0]
 
     count = 0
-    for j in range(1, people + 1):
-        if array[j] > avg:
+    for j in array[1:]:
+        if j > avg:
             count += 1
 
     print(f"{round(count / people * 100, 3)}%")
