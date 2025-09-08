00557d7ea52c2c2c6535688a3de4da64f9b12ea6
김민호 <150920427+minho-git@users.noreply.github.com>
2025-09-08 20:45:53 +0900

    [Silver II] Title: 외판원 순회 2, Time: 116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,16 +1,22 @@
-def travel_city(cost, count, visited, start):
-    global min_cost, N, origin
+import sys
+
+
+def travel_city(cost, count, visited, start, origin):
+    global min_cost, N
     if count == N:
         if board[start][origin] != 0:
             cost += board[start][origin]
             min_cost = min(min_cost, cost)
             return
 
+    if cost > min_cost:
+        return
+
     for i in range(N):
         if not visited[i] and board[start][i] != 0:
             visited[i] = True
 
-            travel_city(cost + board[start][i], count+1, visited, i)
+            travel_city(cost + board[start][i], count+1, visited, i, origin)
 
             visited[i] = False
 
@@ -18,7 +24,7 @@ N = int(input())
 
 origin = 0
 board = []
-min_cost = 10000000
+min_cost = sys.maxsize
 
 for i in range(N):
     board.append(list(map(int, input().split())))
@@ -31,7 +37,8 @@ for i in range(N):
     count = 1
     cost = 0
 
-    travel_city(cost, count, visited_city, i)
+    travel_city(cost, count, visited_city, i, origin)
+    visited_city[i] = False
 
 print(min_cost)
 
