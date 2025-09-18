0e8c93bec48f516c0881cb844a2f1f6201827fe1
김민호 <150920427+minho-git@users.noreply.github.com>
2025-09-18 11:10:43 +0900

    [Silver III] Title: 프린터 큐, Time: 64 ms, Memory: 38076 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -5,35 +5,31 @@ T = int(input())
 
 for i in range(T):
     N, M = map(int, input().split())
-    중요도 = list(map(int, input().split()))
+    prioritys = list(map(int, input().split()))
     max_heap = []
-    큐 = deque()
+    queue = deque()
 
     for i in range(N):
-        # 힙에 저장하자. 
-        heapq.heappush(max_heap, -중요도[i])
+        heapq.heappush(max_heap, -prioritys[i])
 
-        # 큐에 인덱스랑 같이 저장하자.
-        큐.append((i, 중요도[i]))
+        queue.append((i, prioritys[i]))
 
     count = 1
-    힙에서뺀것 = -heapq.heappop(max_heap)
+    popped_heap = -heapq.heappop(max_heap)
 
+    while queue:
+        popped_queue = queue.popleft()
+        index, priority = popped_queue[0], popped_queue[1]
 
-    # 계속 반복하자.
-    while 큐:
-        큐에서뺀것 = 큐.popleft()
-        index, 중요도값 = 큐에서뺀것[0], 큐에서뺀것[1]
-
-        if 중요도값 == 힙에서뺀것:
+        if priority == popped_heap:
             if index == M:
                 print(count)
                 break
             else:
-                힙에서뺀것 = -heapq.heappop(max_heap)
+                popped_heap = -heapq.heappop(max_heap)
                 count += 1
 
 
         else:
-            큐.append(큐에서뺀것)
+            queue.append(popped_queue)
 
