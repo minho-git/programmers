7be8f55e7825b710c85eddf3511e72355d184609
김민호 <150920427+minho-git@users.noreply.github.com>
2025-09-21 18:23:07 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1724 ms, Memory: 100784 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,38 +1,52 @@
 import sys
 from collections import deque
 
+# N(노드 개수), M(에지 개수), K(목표 거리), X(시작점)
 N, M, K, X = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(N+1)]
-distance = [-1] * (N + 1)
-
-for i in range(M):
-    start, end = map(int, sys.stdin.readline().split())
-    graph[start].append((end, 0))
-
-que = deque()
-que.append((X, 0))
-distance[X] = 0
+visited = [-1] * (N + 1) # 각 노드의 최소거리를 입력, -1일때는 방문X
 answer = []
 
-while que:
+# BFS(시작점)
+    # 큐를 만들고, 첫 번째 큐에 시작점을 대입
+    # 초기 거리 설정
 
-    방문도시, 횟수 = que.popleft()
+    # While 큐가 빌때까지:
+        # 큐에서 하나 빼서
+        # 방문할 수 있는 노드들을 반복하면서
+        # 만약 뺀 값이 방문안했다면
+        # 큐에 대입
+        # 거리 설정(이전 값 이용)
 
-    if 횟수 == K:
-        answer.append(방문도시)
+def BFS(v):
+    que = deque()
+    que.append(v)
+    visited[v] = 0
 
-    elif 횟수 < K:
-        for 방문할도시, _ in graph[방문도시]:
-            if distance[방문할도시] == -1:
-                que.append((방문할도시, 횟수 + 1))
-                distance[방문할도시] = 횟수+1
+    while que:
+        current_node = que.popleft()
+        for next_node in graph[current_node]:
+            if visited[next_node] == -1:
+                que.append(next_node)
+                visited[next_node] = visited[current_node] + 1
 
 
-answer.sort()
+for i in range(M):
+    A, B = map(int, sys.stdin.readline().split())
+    graph[A].append(B)
+
+# BFS() 시작
+BFS(X)
+
+for i in range(1, N+1):
+    if visited[i] == K:
+        answer.append(i)
+
 
 if not answer:
     print(-1)
 else:
+    answer.sort()
     for i in answer:
         print(i)
 
