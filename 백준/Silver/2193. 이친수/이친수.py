9b269b9be64f6b6ee5e88f7f3db9dd5a6bf5fd00
김민호 <150920427+minho-git@users.noreply.github.com>
2025-09-27 23:19:51 +0900

    [Silver III] Title: 이친수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -0,0 +1,29 @@
+# D[n] = D[n-1] + D[n-2]
+
+
+#   0  1
+#1  0  1
+#2  1  0
+#3  1  1
+#4  2  1
+#5  3  2
+#6  5  3
+
+n = int(input())
+
+if n == 1:
+    print(1)
+else:
+    
+    D = [0 for i in range(n+1)]
+    D[1] = 1
+    D[2] = 1
+
+    for i in range(2, n+1):
+        D[i] = D[i-1] + D[i-2]
+
+    print(D[n])
+
+
+
+
