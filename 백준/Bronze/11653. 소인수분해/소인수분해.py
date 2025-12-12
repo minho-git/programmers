f77541194cd100b415f70df41582a639ea3d555d
김민호 <150920427+minho-git@users.noreply.github.com>
2025-12-12 10:57:17 +0900

    [Bronze I] Title: 소인수분해, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.py
+++ 백준/Bronze/11653. 소인수분해/소인수분해.py
@@ -1,13 +1,18 @@
 N = int(input())
 
-while (N > 1):
-    
-    for i in range(2, N):
-        if (N % i == 0):
-            print(i)
-            N = N // i
-            break
-    else:
+if N == 1:
+    pass
+else:
+    D = 2
+
+    while (D * D <= N):
+        while (N % D == 0):
+            print(D)
+            N //= D
+        
+        D += 1
+
+if N > 1:
         print(N)
-        break
+
     
