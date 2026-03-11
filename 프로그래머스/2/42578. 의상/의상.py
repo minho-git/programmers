71bd74e907ad6d1b515337c21ece5c603bf145f9
김민호 <150920427+minho-git@users.noreply.github.com>
2026-03-12 00:31:21 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 9.07 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -0,0 +1,21 @@
+def solution(clothes):
+    _dict = {}
+    
+    for cloth in clothes:
+        if _dict.get(cloth[1], 0) == 0:
+            _dict[cloth[1]] = [cloth[0]]
+        else:
+            _dict[cloth[1]].append(cloth[0])
+        # _dict[cloth[1]] = _dict.get(cloth[1], list()).append(cloth[0])
+    
+    res = []
+    for x in _dict.values():
+        res.append(len(x)+1)
+    
+    print(_dict)
+    print(res)
+    total = 1
+    for x in res:
+        total *= x
+    
+    return total - 1
