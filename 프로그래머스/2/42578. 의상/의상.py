0af4e803a10ef9efb6dbf7920019a6e1f2611045
김민호 <150920427+minho-git@users.noreply.github.com>
2026-03-12 00:40:35 +0900

    [level 2] Title: 의상, Time: 0.05 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,21 +1,33 @@
+from collections import Counter
+
 def solution(clothes):
-    _dict = {}
+#     _dict = {}
     
+#     for cloth in clothes:
+#         if _dict.get(cloth[1], 0) == 0:
+#             _dict[cloth[1]] = [cloth[0]]
+#         else:
+#             _dict[cloth[1]].append(cloth[0])
+    
+#     res = []
+#     for x in _dict.values():
+#         res.append(len(x)+1)
+    
+#     print(_dict)
+#     print(res)
+#     total = 1
+#     for x in res:
+#         total *= x
+    
+#     return total - 1
+
+    type = []
     for cloth in clothes:
-        if _dict.get(cloth[1], 0) == 0:
-            _dict[cloth[1]] = [cloth[0]]
-        else:
-            _dict[cloth[1]].append(cloth[0])
-        # _dict[cloth[1]] = _dict.get(cloth[1], list()).append(cloth[0])
+        type.append(cloth[1])
     
-    res = []
-    for x in _dict.values():
-        res.append(len(x)+1)
+    c = list(Counter(type).values())
+    res = 1
+    for x in c:
+        res *= x + 1
     
-    print(_dict)
-    print(res)
-    total = 1
-    for x in res:
-        total *= x
-    
-    return total - 1
+    return res - 1
