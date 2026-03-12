b2801a4a08f70fc5581664b932359d725dbf7dcc
김민호 <150920427+minho-git@users.noreply.github.com>
2026-03-12 17:31:11 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.03 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
@@ -1,23 +1,14 @@
 def solution(s):
+    arr = s.split(" ")
     res = ""
-
-    if s[0].isdigit():
-        res += s[0]
-    elif s[0].isalpha():
-        res += s[0].upper()
-    else:
-        res += s[0]
-        
     
-    for i in range(1, len(s)):
-        if s[i-1] == " ":
-            if s[i].isdigit():
-                res += s[i]
-            elif s[i].isalpha():
-                res += s[i].upper()
-            else:
-                res += s[i]
-        else:
-            res += s[i].lower()        
+    print(arr)
+    
+    for i in range(len(arr)):
+        res += arr[i].capitalize()
+        
+        if i != len(arr) -1:
+            res += " "
+              
             
     return res
--- 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
 
 ### 성능 요약
 
-메모리: 9.23 MB, 시간: 0.05 ms
+메모리: 9.25 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 12일 17:20:18
+2026년 03월 12일 17:31:08
 
 ### 문제 설명
 
