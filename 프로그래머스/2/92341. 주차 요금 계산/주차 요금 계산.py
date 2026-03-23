08431e808e07d11a7f33313069dfb277f0ef6a89
김민호 <150920427+minho-git@users.noreply.github.com>
2026-03-24 02:36:12 +0900

    [level 2] Title: 주차 요금 계산, Time: 1.96 ms, Memory: 9.61 MB -BaekjoonHub

---
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -0,0 +1,50 @@
+def solution(fees, records):
+    for i in range(len(records)):
+        tmp = records[i].split()
+        time = tmp[0].split(":")
+        hour = int(time[0])
+        minute = int(time[1])
+        total_minute =  hour * 60 + minute
+        tmp[0] = total_minute
+        
+        records[i] = tmp
+        # [분, 번호, 인/아웃]
+        
+    answer = []
+    cars = {}
+    default = 60 * 23 + 59
+ 
+    for i in range(len(records)):
+        info = records[i]
+        
+        if info[2] == "IN":
+            cars[info[1]] = cars.get(info[1], 0) - info[0]
+        else:
+            cars[info[1]] = cars.get(info[1], 0) + info[0]
+
+    
+
+    for key, value in cars.items():
+        if value <= 0:
+            cars[key] += default
+        
+    
+    for key, value in cars.items():
+        cars[key] = value - fees[0]
+        
+        if cars[key] < 0:
+            cars[key] = 0
+            cars[key] = fees[1]
+        else:
+            if cars[key] % fees[2] == 0:
+                cars[key] = fees[1] + int(cars[key] / fees[2]) * fees[3]
+            else:
+                cars[key] = fees[1] + int((cars[key] / fees[2]) + 1) * fees[3]
+    
+    
+    array = sorted(cars)
+    for key in array:
+        answer.append(cars[key])
+    
+    
+    return answer
