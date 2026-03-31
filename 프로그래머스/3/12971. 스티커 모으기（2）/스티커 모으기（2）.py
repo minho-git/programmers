aa4ba4defd347f4d276738d4d5e6568a0ba1e795
김민호 <150920427+minho-git@users.noreply.github.com>
2026-03-31 13:51:06 +0900

    [level 3] Title: 스티커 모으기(2), Time: 62.88 ms, Memory: 15.8 MB -BaekjoonHub

--- 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,27 +1,20 @@
 def solution(sticker):
-    answer = 0
-
+    
+    if len(sticker) == 1:
+        return sticker[0]
+    
     dp1 = [0] * len(sticker)
     dp2 = [0] * len(sticker)
 
     # 첫 번째 스티커를 땟을 때
-    for i in range(len(sticker)-1):
-        if i == 0:
-            dp1[0] = sticker[0]
-        elif i == 1:
-            dp1[1] = max(dp1[0], sticker[1])
-        else:
-            dp1[i] = max(dp1[i-1], dp1[i-2] + sticker[i])
+    dp1[0] = sticker[0]
+    dp1[1] = max(dp1[0], sticker[1])
+    for i in range(2, len(sticker)-1):
+        dp1[i] = max(dp1[i-1], dp1[i-2] + sticker[i])
     
     # 첫 번째 스티커를 안 땟을 때
-    for i in range(1, len(sticker)):
-        if i == 1:
-            dp2[1] = sticker[1]
-        else:
-            dp2[i] = max(dp2[i-1], dp2[i-2] + sticker[i])
+    dp2[1] = sticker[1]
+    for i in range(2, len(sticker)):
+        dp2[i] = max(dp2[i-1], dp2[i-2] + sticker[i])
 
-    if len(sticker) == 1:
-        answer = sticker[0]
-    else:
-        answer = max(max(dp1), max(dp2))    
-    return answer
+    return max(max(dp1), max(dp2))    
