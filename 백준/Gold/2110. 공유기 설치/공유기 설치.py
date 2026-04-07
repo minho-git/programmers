d579d37ce033e47db7362688f63eb137fac1ea2e
김민호 <150920427+minho-git@users.noreply.github.com>
2026-04-07 14:06:14 +0900

    [Gold IV] Title: 공유기 설치, Time: 5636 ms, Memory: 41164 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,42 +1,31 @@
-import sys
-
 N, C = map(int, input().split())
+graph = []
+answer = 1
 
-# 1. 집을 입력받고, 크기 순으로 정렬한다.
-houses = []
 for i in range(N):
-    houses.append(int(sys.stdin.readline()))
+    graph.append(int(input()))
 
-houses.sort()
+graph.sort()
 
 start = 1
-end = houses[-1] - houses[0]
-answer = 0
+end = graph[-1] - graph[0]
 
-# 2. 가장 작은 값부터 가장 큰 값까지 mid 값으로 더해본다.
 while start <= end:
     mid = (start + end) // 2
-    last = houses[0]
-
-    # 첫 번째 집에 공유기 설치 했으니깐 count는 1부터 시작
     count = 1
+    tmp = mid
 
     for i in range(1, N):
-        if houses[i] - last >= mid:
+        tmp -= (graph[i] - graph[i-1])
+
+        if tmp <= 0:
             count += 1
-            last = houses[i]
+            tmp = mid
 
     if count >= C:
         start = mid + 1
-        answer = max(answer, mid)
-    elif count < C:
+        answer = mid
+    else:
         end = mid - 1
 
-
 print(answer)
-
-
-
-
-
-
