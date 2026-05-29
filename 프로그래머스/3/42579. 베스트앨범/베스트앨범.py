1930ef8a35e310086e2e0041bcd169b9de572704
김민호 <150920427+minho-git@users.noreply.github.com>
2026-05-29 11:54:09 +0900

    [level 3] Title: 베스트앨범, Time: 0.04 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,34 +1,30 @@
 def solution(genre_array, play_array):
-    # 우선순위 정렬
-    genre = {}
-    for i in range(len(genre_array)):
-        if not genre.get(genre_array[i]):
-            genre[genre_array[i]] = play_array[i]
-        else:
-            genre[genre_array[i]] += play_array[i]
 
-    genre_sorted_array = sorted(genre.items(), key=lambda item:item[1], reverse=True)
-
-    # key(장르) : value((고유번호, 횟수))
-    album = {}
-    for i in range(len(genre_sorted_array)):
-        album[genre_sorted_array[i][0]] = []
-
-    for i in range(len(genre_array)):
-            album[genre_array[i]].append((i, play_array[i]))
-
-    for key in album.keys():
-        album[key] = sorted(album.get(key), key = lambda x:x[1], reverse=True)
-
+    _dict = {}
+    songs = {}
     result = []
-
-    for key in album.keys():
-        if len(album.get(key)) == 1:
-            result.append(album.get(key)[0][0])
-
+    
+    for i in range(len(genre_array)):
+        _dict[genre_array[i]] = _dict.get(genre_array[i], 0) + play_array[i]
+    
+    sorted_dict = sorted(_dict, key = lambda x : _dict[x], reverse=True)
+  
+    for genre in sorted_dict:
+        songs[genre] = []
+        
+    for i in range(len(genre_array)):
+        songs[genre_array[i]].append((i, play_array[i]))
+        
+    for value in songs.values():
+        value.sort(key = lambda x : (x[1], -x[0]), reverse = True)
+    
+    for genre in sorted_dict:
+        if len(songs[genre]) == 1:
+            result.append(songs[genre][0][0])
         else:
-            result.append(album.get(key)[0][0])
-            result.append(album.get(key)[1][0])
-
-
+            result.append(songs[genre][0][0])
+            result.append(songs[genre][1][0])
+    
     return result
+
+    
