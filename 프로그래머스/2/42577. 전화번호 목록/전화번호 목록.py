4bad1df67ef7b9f261541dae9741de738bc577ac
김민호 <150920427+minho-git@users.noreply.github.com>
2026-05-29 10:43:59 +0900

    [level 2] Title: 전화번호 목록, Time: 49.72 ms, Memory: 28.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,30 +1,10 @@
 def solution(phone_book):
-    phone_book.sort(key = lambda x:len(x))
-    phone_book.reverse()
+    phone_book.sort()
     
-    _set = set()
-    
-    for phone_num in phone_book:
-        if phone_num in _set:
-            return False
+    for i in range(len(phone_book) - 1):
         
-        for i in range(1, len(phone_num)):
-            _set.add(phone_num[:i])
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
         
     
     return True
-        
-        
-#     while phone_book:
-#         popped = phone_book.pop(0)
-
-#         for phone in phone_book:
-
-#             _length = len(popped)
-#             if _length > len(phone):
-#                 continue
-
-#             if popped == phone[0:_length]:
-#                 return False
-        
-#     return True
