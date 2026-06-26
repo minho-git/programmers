ff0453cafbebf063c4998ce6d99aa238fa3aded6
김민호 <150920427+minho-git@users.noreply.github.com>
2026-06-26 11:41:32 +0900

    [level 2] Title: 괄호 회전하기, Time: 28.82 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=python3) 
 
 ### 성능 요약
 
-메모리: 9.25 MB, 시간: 105.63 ms
+메모리: 11.3 MB, 시간: 28.82 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 25일 01:45:12
+2026년 06월 26일 11:41:29
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -1,42 +1,40 @@
 def check(s):
-    tmp = list(s)
-    stack = []
-    while tmp:
-        _popped = tmp.pop()
-        if _popped in (")", "}", "]"):
-            stack.append(_popped)
+    tmp = list()
+    
+    for i in s:
+        if i in ["(", "{", "["]:
+            tmp.append(i)
         else:
-            if len(stack) == 0:
+            if not tmp:
                 return False
-            
-            if _popped == "(":
-                if stack.pop() == ")":
-                    continue
-                else:
-                    return False
-            elif _popped == "{":
-                if stack.pop() == "}":
-                    continue
-                else:
-                    return False
-            elif _popped == "[":
-                if stack.pop() == "]":
-                    continue
-                else:
-                    return False
-                
-    if len(stack) > 0:
+            elif i == ")":
+                if tmp[-1] == "(":
+                    tmp.pop()
+            elif i == "}":
+                if tmp[-1] == "{":
+                    tmp.pop()
+            elif i == "]":
+                if tmp[-1] == "[":
+                    tmp.pop()
+        
+    
+    if tmp:
         return False
     else:
         return True
-    
+                
+                
+
 def solution(s):
     answer = 0
     
     for i in range(len(s)):
-        tmp = s[i:len(s)] + s[0:i]
-        if check(tmp):
+        # 새로운 문자열 만들기
+        new_s = s[i:] + s[:i]
+        
+        # 문자열 체크 함수 호출하기
+        if check(new_s):
             answer += 1
-            
-    
+
     return answer
+
