aeafbc6a31230739f3964385a92fe59880d6fc0e
김민호 <150920427+minho-git@users.noreply.github.com>
2026-06-26 10:05:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 41.00 ms, Memory: 30.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=python3) 
 
 ### 성능 요약
 
-메모리: 38 MB, 시간: 75.83 ms
+메모리: 30.5 MB, 시간: 41.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 12일 00:05:23
+2026년 06월 26일 10:05:15
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,20 @@
 from collections import Counter
 
 def solution(participant, completion):
+    tmp = {}
+    answer = ""
 
-    dif = Counter(participant) - Counter(completion)
-    dif = list(dif.keys())[0]
-
-    return dif
+    for a in participant:
+        tmp[a] = tmp.get(a, 0) + 1
+    
+    
+    for b in completion:
+        if tmp.get(b) > 1:
+            tmp[b] = tmp[b] - 1
+        else:
+            del tmp[b]
+    
+    for key in tmp.keys():
+        answer = key
+        
+    return answer
