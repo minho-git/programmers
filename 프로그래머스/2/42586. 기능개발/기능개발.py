0488bad87bc771c2b2ce1638fc9351e8bc706584
김민호 <150920427+minho-git@users.noreply.github.com>
2026-06-28 20:36:12 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,33 @@
-import math
+from collections import deque
 
 def solution(progresses, speeds):
-    
-    days = []
+    q = deque()
+    day = 0
     answer = []
-
-    for i in range(len(progresses)):
-        remains = 100 - progresses[i]
-        days.append(math.ceil(remains / speeds[i]))
+    
+    for i, progress in enumerate(progresses):
+        q.append((i, progress))
+    
+    
+    while q:
+        count = 1
+        index, current = q.popleft()
+        current += day * speeds[index]
         
-    now = days[0]
-    count = 1
-    for i in range(1, len(days)):
-        if days[i] <= now:
-            count += 1
-        else:
-            answer.append(count)
-            now = days[i]
-            count = 1
-            
-    answer.append(count)
+        while current < 100:
+            day += 1
+            current += speeds[index]
+        
+        while q:
+            next = q.popleft()
+            if next[1] + speeds[next[0]] * day >= 100:
+                count += 1
+            else:
+                q.appendleft(next)
+                break
+                
+        
+        answer.append(count)
+        
+    
     return answer
