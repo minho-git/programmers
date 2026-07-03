7e6fdbf9ec73dff6d8415012fd36735f0a376bd4
김민호 <150920427+minho-git@users.noreply.github.com>
2026-07-03 12:36:27 +0900

    [level 2] Title: 가장 큰 수, Time: 133.85 ms, Memory: 25.6 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,22 +1,19 @@
-from functools import cmp_to_key 
+from functools import cmp_to_key
 
 def compare(a, b):
     if a + b > b + a:
         return -1
     else:
         return 1
-    
 
 def solution(numbers):
     
     numbers_str = [str(number) for number in numbers]
     numbers_str.sort(key=cmp_to_key(compare))
     
-    
-    answer = ''
+    answer = ""
     for number_str in numbers_str:
         answer += number_str
-        
         
     if answer[0] == "0":
         answer = "0"
