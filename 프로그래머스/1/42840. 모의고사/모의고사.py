2d04200db6d8ae9d020ffa4cb1e7924ab2ef5a8b
김민호 <150920427+minho-git@users.noreply.github.com>
2026-07-04 13:30:31 +0900

    [level 1] Title: 모의고사, Time: 2.47 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,69 +1,23 @@
 def solution(answers):
     
-    grade1 = 0
-    grade2 = 0
-    grade3 = 0
-
-    for i in range(len(answers)):
-        if i % 5 == 0:
-            if answers[i] == 1:
-                grade1 += 1
-        elif i % 5 == 1:
-            if answers[i] == 2:
-                grade1 += 1
-        elif i % 5 == 2:
-            if answers[i] == 3:
-                grade1 += 1
-        elif i % 5 == 3:
-            if answers[i] == 4:
-                grade1 += 1
-        elif i % 5 == 4:
-            if answers[i] == 5:
-                grade1 += 1
+    pattern = [
+        [1, 2, 3, 4, 5], # 5
+        [2, 1, 2, 3, 2, 4, 2, 5], # 8
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5] # 10
+    ]
     
-    for i in range(len(answers)):
-        if i % 2 == 0:
-            if answers[i] == 2:
-                grade2 += 1
-        elif i % 8 == 1:
-            if answers[i] == 1:
-                grade2 += 1
-        elif i % 8 == 3:
-            if answers[i] == 3:
-                grade2 += 1
-        elif i % 8 == 5:
-            if answers[i] == 4:
-                grade2 += 1
-        elif i % 8 == 7:
-            if answers[i] == 5:
-                grade2 += 1            
+    result = []
+    grades = [0, 0, 0]
     
-    for i in range(len(answers)):
-        v = i % 10
-        if v in [0, 1]:
-            if answers[i] == 3:
-                grade3 += 1
-        elif v in [2, 3]:
-            if answers[i] == 1:
-                grade3 += 1
-        elif v in [4, 5]:
-            if answers[i] == 2:
-                grade3 += 1
-        elif v in [6, 7]:
-            if answers[i] == 4:
-                grade3 += 1
-        elif v in [8, 9]:
-            if answers[i] == 5:
-                grade3 += 1      
-        
-    answer = []
+    for index, value in enumerate(answers):
+        for i in range(3):
+            if value == pattern[i][index % len(pattern[i])]:
+                grades[i] += 1
     
-    grades = [grade1, grade2, grade3]
-    max_value = max(grades)
+    max_grade = max(grades)
+    for index, value in enumerate(grades):
+        if value == max_grade:
+            result.append(index+1)
     
-    
-    for i in range(len(grades)):
-        if grades[i] == max_value:
-            answer.append(i+1)
-    answer.sort()
-    return answer
+    result.sort()
+    return result
