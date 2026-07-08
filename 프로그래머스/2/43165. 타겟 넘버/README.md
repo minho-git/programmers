9646225519e7864cbc026c651cc00938caa136c1
김민호 <150920427+minho-git@users.noreply.github.com>
2026-07-08 19:26:21 +0900

    [level 2] Title: 타겟 넘버, Time: 127.85 ms, Memory: 11.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 9.11 MB, 시간: 528.58 ms
+메모리: 11.5 MB, 시간: 127.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 20일 15:14:44
+2026년 07월 08일 19:26:16
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,22 +1,17 @@
-def solution(numbers, target):
-    answer = get_count_of_ways_to_target_by_doing_plus_or_minus(numbers, target)
+def dfs(index, result, array, value):
+    
+    if index == len(array):
+        if result == value:
+            return 1
+    
+        return 0
+    
+    
+    # 각 인덱스의 있는 값을 더하거나 빼거나 한다.
+    return dfs(index+1, result + array[index], array, value) + dfs(index+1, result - array[index], array, value)
+
+def solution(numbers, target): 
+    
+    answer = dfs(0, 0, numbers, target)
+
     return answer
-
-def get_count_of_ways_to_target_by_doing_plus_or_minus(array, target):
-    count = 0
-    result = 0
-    index = 0
-
-    def dfs(index, result):
-        if index == len(array):
-            if result == target:
-                nonlocal count
-                count += 1
-            return
-        else:
-            dfs(index + 1, result + array[index])
-            dfs(index + 1, result - array[index])
-
-    dfs(index, result)
-
-    return count
