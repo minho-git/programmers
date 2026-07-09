bdee11b0d25b1f4ff8c8c724e5d95164023e3768
김민호 <150920427+minho-git@users.noreply.github.com>
2026-07-09 10:02:34 +0900

    [level 3] Title: 네트워크, Time: 0.53 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,21 +1,21 @@
-def dfs(y, x, visited, computers):
+def dfs(y, visited, computers):
     
     for i in range(len(computers)):
-        if computers[x][i] == 1 and visited[x][i] == 0:
-            visited[x][i] = 1
-            dfs(x, i, visited, computers)
+        if computers[y][i] == 1 and visited[i] == 0:
+            visited[i] = 1
+            dfs(i, visited, computers)
             
     
 
 def solution(n, computers):
     answer = 0
-    visited = [(n) * [0] for _ in range(n)]
+    visited = n * [0]
     
     for i in range(n):
         for j in range(n):
-        
-            if computers[i][j] == 1 and visited[i][j] == 0:
-                dfs(i, j, visited, computers)
+            if computers[i][j] == 1 and visited[j] == 0:
+                visited[j] = 1
+                dfs(j, visited, computers)
                 answer += 1
 
     return answer
