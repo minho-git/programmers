26573237e96c70340173799f7b9b64392b0312aa
김민호 <150920427+minho-git@users.noreply.github.com>
2026-07-15 11:14:09 +0900

    [level 4] Title: 징검다리, Time: 91.11 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,40 +1,46 @@
 def solution(distance, rocks, n):
-    answer = 0
+    # 이 문제는 일단 이분 탐색임을 인지한다.
+    # 거리의 최솟값의 최댓값을 구해라. 및 범위가 1,000,000,000
+    # 이분탐색!
+    
+    # 최소, 최대를 구해야한다.
+    # 최소는 1, 최대는 distance
+    
     rocks.append(0)
     rocks.append(distance)
     rocks.sort()
-   
-    # 거리의 최솟값을 줄여나가보자.
-    # 해당 거리일때 루프를 돌면서 바위를 몇개 제거해야하는지 체크
-    # 바위를 n개보다 많이 제거해야한다 -> 실패 -> end = mid - 1
-    # 바위를 n개보다 같거나 적게 제거했다 -> 성공 -> start = mid + 1
-    # 우리가 구할게 거리의 최솟값의 최댓값이므로
-    # 성공할때마다 최솟값의 최댓값을 구하자.
+    
     start = 1
     end = distance
+    answer = 0
     
     while start <= end:
         mid = (start + end) // 2
-        count = 0
+        _min = distance
         
+        # 여기서 돌 몇 개 제거가능한지 체크
+        # prev를 꼭 기억해두자. 왜냐하면 돌을 제거했으면 과거의 돌과 거리를 비교해야하기때문에
         prev = 0
+        count = 0
         
         for i in range(1, len(rocks)):
             tmp = rocks[i] - prev
             
-            if tmp < mid: # 돌이 제거되야한다.
+            if tmp < mid:
                 count += 1
             else:
                 prev = rocks[i]
+                _min = min(_min, tmp)
         
-        if count <= n:
-            start = mid + 1
-            answer = max(answer, mid)
+        if count > n:
+            end = mid - 1
         else:
-            end = mid -1
-        
+            start = mid + 1
+            answer = max(answer, _min)
+            
+            
                 
-        
+            
     
     
     return answer
